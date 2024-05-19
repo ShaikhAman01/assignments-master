9ef8355d611d9cccb4254abd59a92c05458e1baa
Shaikh Aman <110104595+ShaikhAman01@users.noreply.github.com>
2024-05-19 18:51:13 +0530

    complete assignments

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
-
+//mongodb+srv://admin:7uhiOs4zMCKcBe5i@cluster0.pxs0hlx.mongodb.net/
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://admin:7uhiOs4zMCKcBe5i@cluster0.pxs0hlx.mongodb.net/course_selling_app');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username:String,
+    password:String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'course'
+    }]
 });
+
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title:String,
+    description:String,
+    price:Number,
+    imageLink:String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,20 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+const { User } = require("../db")
+
+async function userMiddleware(req, res, next) {
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+    const password = req.headers.password;
+    
+        const user = await User.findOne({ username, password });
+        if (user) {
+            next();
+        } else {
+            res.status(403).json({
+                msg: "Incorrect credentials or User doesn't exist"
+            });
+        }
+     
 }
 
 module.exports = userMiddleware;
